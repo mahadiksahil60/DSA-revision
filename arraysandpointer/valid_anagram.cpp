66d67014dea8fe725b519c069d38fde3f7dd0a2c
sahilm-sankey <sahil.m1@sankeysolutions.com>
2026-09-12 16:25:43 +0530

    valid anagram problem solved

---
+++ arraysandpointer/valid_anagram.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+bool isAnagram(string s, string t) {
+    
+    // use freq map
+    unordered_map<char, int> freqMap;
+    unordered_map<char, int> freqMap2;
+    
+    if (s.length() != t.length()) return false;
+    // This loop for storing every element in the freq map 
+    for (int i = 0; i < s.length(); i++) { 
+        freqMap[s[i]]++;
+    };
+
+    // this loop for storing every element of the second string 
+    for (int i=0;i<t.length(); i++) { 
+        freqMap2[t[i]]++;
+    }
+
+    for (const auto& pair : freqMap) {
+        auto it = freqMap2.find(pair.first);
+        
+        // Check if the key is missing or if the values don't match
+        if (it == freqMap2.end() || it->second != pair.second) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() { 
+    string s = "anagram";
+    string t = "nagaram";
+    cout << "hello" << endl;
+    bool ans = isAnagram(s, t);
+    cout << ans << endl;
+}
