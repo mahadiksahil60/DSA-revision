f9e452d51e0c46fea745d691c0620df881aa1a8b
sahilm-sankey <sahil.m1@sankeysolutions.com>
2026-09-22 09:03:41 +0530

    valid parenthesis

--- neetcode150/stack/valid_parenthesis.cpp
+++ neetcode150/stack/valid_parenthesis.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/// @brief Appraoch 1 : create a stack and an unordered map for storing opp pair of brackets. 
+// thorugh a loop insert if its a opening brancket. if its a closing brached, check if a corresponting closing branked exists in the mop
+/// @param s 
+/// @return 
 bool isValid(string s) {
-    
+    stack<char> st;
+	unordered_map<char, char> match = {{')', '('}, {']', '['}, {'}', '{'}};
+	for (char c : s) { 
+		if (c == '{' || c == '[' || c == '(') { 
+			st.push(c);
+		}
+		else { 
+			if (st.empty() || st.top() != match[c]) return false;
+			st.pop();
+		}
+	}
+
+	return st.empty();
 }
 
 int main()
@@ -10,4 +26,5 @@ int main()
 	string s = "()";
 	bool ans = isValid(s);
 	cout << ans << endl;
+	cout <<"EOL"<<endl;
 }
